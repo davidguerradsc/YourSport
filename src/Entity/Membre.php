bcb381ae10a2a7ce2661340e3a18721a664d78be
YourSport <48954800+YourSport@users.noreply.github.com>
2019-05-09 15:47:59 +0200

    Update suppression compte

--- src/Entity/Membre.php
+++ src/Entity/Membre.php
@@ -331,10 +331,6 @@ class Membre implements UserInterface
         return $this->email;
     }
 
-    public function __toString()
-    {
-        return $this->date_de_naissance;
-    }
 
     /**
      * Removes sensitive data from the user.
